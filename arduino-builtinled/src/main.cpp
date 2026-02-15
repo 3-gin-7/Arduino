84a1b6721628271fa4160ff3f8810a4bc574679f
gin <gin@gin.com>
2026-02-15 18:48:31 +0200

    first commit

---
+++ arduino-builtinled/src/main.cpp
@@ -0,0 +1,11 @@
+#include "pins_arduino.h"
+#include <Arduino.h>
+
+void setup() { pinMode(LED_BUILTIN, OUTPUT); }
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
+}
